73646757c874a8a070d960c40c6fdd2f0928bc2f
MiquelVera <mivera@ucm.es>
2020-05-23 19:02:06 +0200

    CreatedTest

--- src/components/pages/CreatedTests.js
+++ src/components/pages/CreatedTests.js
@@ -2,25 +2,46 @@ import React, { Component } from 'react';
 import './pages.css';
 import questionMarkBlue from '../../QuestionMark(blue).png';
 
+import { Link } from 'react-router-dom';
+
 class CreatedTests extends Component {
+
+    constructor() {
+        super();
+        this.tests =[{name: 'Test 1', link: '/created-tests'},
+                     {name: 'Test 2', link: '/created-tests'},
+                     {name: 'Test 2', link: '/created-tests'},
+                     {name: 'Test 2', link: '/created-tests'},
+                     {name: 'Test 2', link: '/created-tests'},
+                     {name: 'Test 2', link: '/created-tests'},];
+    }
+
     render() {
+
+        
+
         return (
         <div className="page">
             <div className="component">
                 <div className="row mr-auto">
                     <div className="created col-lg-6 col-md-12">
-                        <h1>CreatedTests page</h1>
+                        {
+                        this.tests.map(opcion => {
+                            return(
+                                <Link to={opcion.link}><button className="btn btn-custom-black" >{opcion.name}</button></Link>
+                            );
+                        })
+                        }
                     </div>
 
                     <div className="create col-lg-6 col-md-12">
-                        <div className="create-image">
-                            <h1>Image</h1>
-                            <img className="questionMarkImage" src={questionMarkBlue} alt="questionMarkImage"/>
+                        <div className="create-button">
+                            <button className="btn btn-custom-blue">+ New test</button>
                         </div>
                         <br/>
                         <br/>
-                        <div className="create-button">
-                            <button className="btn btn-custom">+ New test</button>
+                        <div className="create-image">
+                            <img className="questionMarkImage" src={questionMarkBlue} alt="questionMarkImage"/>
                         </div>
                     </div>
                 </div>
