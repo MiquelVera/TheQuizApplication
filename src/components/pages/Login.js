dd844f816d3b09fd6a2e5d08a01486eb1f556c3b
gmartind <65733734+gmartind@users.noreply.github.com>
2020-05-23 13:13:32 +0200

    Add files via upload

---
+++ src/components/pages/Login.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './pages.css';
+
+class Login extends Component {
+    render() {
+        return (
+        <div className="page">
+            <h1>Login</h1>
+        </div>
+        );
+    }
+}
+
+export default Login;
